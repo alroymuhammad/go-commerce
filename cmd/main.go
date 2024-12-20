05aaac30393991f46a0f8c2527429be8efe75416
Muhammad Alroy <alroymuhammad2@gmail.com>
2024-12-20 16:51:40 +0700

    lanjutan commerce

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "admin"
+	dbname   = "commerce"
+)
+
 func main() {
-	addr := flag.String("addr", ":8080", "HTTP network address")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Ping failed: %v", err)
+	}
+	fmt.Println("Successfully connected to the database!")
+	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", http.HandlerFunc(home))
+	mux.HandleFunc("/", home)
 
 	log.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Go Commerce!")
 }
