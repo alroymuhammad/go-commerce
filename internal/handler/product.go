754abb68922a9c3f0e30c33c4764d7ac99bdd7d8
Muhammad Alroy <alroymuhammad2@gmail.com>
2025-01-03 14:59:11 +0700

    comment removal

--- internal/handler/product.go
+++ internal/handler/product.go
@@ -23,7 +23,7 @@ type ProductResponse struct {
 	ID          int64     `json:"id"`
 	Name        string    `json:"name"`
 	Description string    `json:"description"`
-	Price       float64   `json:"price"` // Changed from int64 to float64
+	Price       float64   `json:"price"`
 	Stock       int       `json:"stock"`
 	CreatedAt   time.Time `json:"created_at"`
 }
